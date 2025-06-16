3582d0875e8f8b0b7fd0032e6833812a51e0120c
Hidden_Angle <Rahul5g3d.official@gmail.com>
2025-06-16 23:52:00 +0530

    minor changes in Mission

--- src/Pages/mission.jsx
+++ src/Pages/mission.jsx
@@ -4,7 +4,7 @@ const Mission = () => {
   return (
     <div className="w-full bg-white rounded-lg p-6 shadow-md text-center">
       <h2 className="text-2xl font-bold text-gray-900 mb-4">Our Mission</h2>
-      <p className="text-gray-600">Updating Soon...</p>
+      <p className="text-gray-600">Coming soon, Stay Updated to know more...</p>
     </div>
   );
 };
